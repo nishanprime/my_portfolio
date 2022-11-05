b41c51b3ff2660625fcc4d3f87124c5478171308
nishanprime <thapanishan9@gmail.com>
2022-11-04 20:00:46 -0400

    change photov2

--- components/SkillCard.jsx
+++ components/SkillCard.jsx
@@ -5,7 +5,7 @@ const SkillCard = ({ imgLink, skillName }) => {
     <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
       <div className="grid grid-cols-2 gap-4 justify-center items-center">
         <div className="m-auto">
-          <img src={imgLink} height={64} width={64} />
+          <img src={imgLink} height={64} width={64} alt="/" />
         </div>
         <div className="flex flex-col items-center justify-center">
           <h3>{skillName}</h3>
